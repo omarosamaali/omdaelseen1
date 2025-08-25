7713badd0d1827ab3bb2fa3f23088e0e5b78e440
unknown <987omar123osama456@gmail.com>
2025-08-26 00:28:45 +0300

    omar

--- resources/views/admin/omdaHome/branches/index.blade.php
+++ resources/views/admin/omdaHome/branches/index.blade.php
@@ -80,7 +80,7 @@
 @foreach ($branches as $branche)
     <tr class="odd:bg-white even:bg-gray-50 border-b border-gray-200">
         <td class="th">
-            {{ $branche->id }}
+            {{ $branche->index + 1 }}
         </td>
         <td class="th">
             {{ $branche->name_ar }}
